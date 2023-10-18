13eaa455ce2e4908ee9043bcf8c11da73ca373eb
Jalena Austin <j.da.xmaster@gmail.com>
2023-10-18 17:18:00 -0400

    align calendar and chart

--- client/src/pages/DashboardPage.js
+++ client/src/pages/DashboardPage.js
@@ -7,17 +7,19 @@ export const DashboardPage = () => {
       <div>
         <Navbar />
         
-        <div className='container'>
+          <div className='container'>
 
-          <h1 className='mt-3'>Welcome back!</h1>
+            <h1 className='mt-3'>Welcome back!</h1>
 
-          <p> This is your dashboard. Here is a quick overview of some important things. </p>
-        </div>
-        <div>
-          <PieChartTransactions/>
-        </div>
-        <div>
-          <CalendarComp/>
+            <p> This is your dashboard. Here is a quick overview of some important things. </p>
+          </div>
+          <div style={{display: 'flex'}}>
+            <div style={{flex: 1}}>
+              <PieChartTransactions/>
+            </div>
+          <div style={{flex: 1}}>
+            <CalendarComp/>
+          </div>
         </div>
       </div>
     )
