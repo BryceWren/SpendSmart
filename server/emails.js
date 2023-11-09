860c03b479b8336cddbcbf6d8af5bb5092048442
Addison MacPhee <amacphee04@gmail.com>
2023-11-09 16:10:07 -0500

    email smtp stuff (UPDATE ENV FILE)

---
+++ server/emails.js
@@ -0,0 +1,33 @@
+// SMTP CONNECTION
+const { createTransport } = require('nodemailer');
+require('dotenv').config();
+
+
+const transporter = createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+    },
+});
+
+
+const sendMail = (email, subject, body) => {
+    const mailOptions = {
+        from: 'do-not-reply@spendsmart.com',
+        to: email,
+        subject: subject,
+        text: body
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
+
+sendMail("addirm@yahoo.com", "test sub", "body of email")
