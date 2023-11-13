3e0a6d4647497c506db07fbd41433f6078e65ed6
Jalena Austin <j.da.xmaster@gmail.com>
2023-11-13 11:49:38 -0500

    arrange edit income btn

--- client/src/components/Income.js
+++ client/src/components/Income.js
@@ -4,8 +4,8 @@ const Income = () => {
 	return (
 		<div className='alert alert-success'>
 			<span>Income: $3000</span>
-			<button type="button" className="btn btn-primary">
-				Edit
+			<button type="button" className="btn btn-primary position-absolute top-50 end-0 translate-middle-y">
+				Edit Your Income
 			</button>
 		</div>
 	);
