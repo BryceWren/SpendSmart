651b809e1bc7347761833c0f139b149c763183a9
Maureen <dunnm2@g.cofc.edu>
2023-11-16 17:59:08 -0500

    User should be able to edit income

--- client/src/components/Income.js
+++ client/src/components/Income.js
@@ -1,14 +1,42 @@
-const Income = () => {
-	//TODO: change to call to server
+import React, { useState } from 'react';
+import IncomeEdit from './IncomeEdit';
 
-	return (
-		<div className='alert alert-success'>
-			<span>Income: $3000</span>
-			<button type="button" className="btn btn-primary position-absolute top-50 end-0 translate-middle-y">
-				Edit Your Income
-			</button>
-		</div>
-	);
+const Income = () => {
+  const [editing, setEditing] = useState(false);
+  const [income, setIncome] = useState(0);
+
+  const handleEditClick = () => {
+    setEditing(true);
+  };
+
+  const handleSaveClick = (newIncome) => {
+    // TODO: Add logic to send the updated income to the server
+    setIncome(newIncome);
+    setEditing(false);
+  };
+
+  const handleCancelClick = () => {
+    setEditing(false);
+  };
+
+  return (
+    <div className='alert alert-success'>
+      {editing ? (
+        <IncomeEdit income={income} handleSaveClick={handleSaveClick} handleCancelClick={handleCancelClick} />
+      ) : (
+        <div>
+          <span>Income: ${income}</span>
+          <button
+            type="button"
+            className="btn btn-primary position-absolute top-50 end-0 translate-middle-y"
+            onClick={handleEditClick}
+          >
+            Edit Your Income
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Income;
